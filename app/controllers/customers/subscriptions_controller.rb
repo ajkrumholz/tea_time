61e3dc31b92b44fb32082eb087146457c73ace60
A.J. Krumholz <krumholz.alan@gmail.com>
2023-01-09 16:02:47 -0500

    create migrations and model for customer

---
+++ app/controllers/customers/subscriptions_controller.rb
@@ -0,0 +1,5 @@
+class Customers::SubscriptionsController < ApplicationController
+  def create
+    require 'pry'; binding.pry
+  end
+end
--- spec/requests/subscriptions/create_spec.rb
+++ spec/requests/subscriptions/create_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe 'when a POST request is sent to /subscriptions' do
+RSpec.describe 'when a POST request is sent to /customer/{customer_id}/subscriptions' do
   describe 'happy path' do
     describe 'when all required fields are present' do
       it 'returns status code 201 and a serialized response' do
+        customer = Customer.new(
+          first_name = "Gary",
+          last_name = "Busey",
+          email = "test@gmail.com",
+          address = "123 America St., Colorado City, CO"
+        )
+        
         body = {
-          title: '',
+          title: 'Mona Lisa Tea Wizards',
           price: 45.99,
           status: 'active',
           frequency: 'monthly',
@@ -13,7 +20,7 @@ RSpec.describe 'when a POST request is sent to /subscriptions' do
           tea_ids: [1, 2]
         }
 
-        post '/subscriptions', params: { body: body.to_json }
+        post customer_subscriptions_path(1), params: { body: body }
 
         result = JSON.parse(response.body, symbolize_names: true)
         expect(result).to be_a Hash
