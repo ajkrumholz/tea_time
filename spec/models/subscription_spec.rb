06de906ef24b5be4cb46175d647343bba251c820
A.J. Krumholz <krumholz.alan@gmail.com>
2023-01-09 16:44:13 -0500

    define and test model relationships

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Subscription do
-  it { should have_many customer_subscriptions }
-  it { should have_many(customers).through customer_subscriptions }
+  it { should have_many :customer_subscriptions }
+  it { should have_many(:customers).through :customer_subscriptions }
 end
