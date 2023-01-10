ca4ee2d6743e7a1e332d9dda3778a4968f183349
A.J. Krumholz <krumholz.alan@gmail.com>
2023-01-09 20:22:24 -0500

    test sad path for bad status

--- spec/requests/customer_subscriptions/update_spec.rb
+++ spec/requests/customer_subscriptions/update_spec.rb
@@ -9,12 +9,13 @@ RSpec.describe 'updating a customer_subscription' do
       it 'allows the subscription status to change to cancelled without deleting the record' do
         customer.subscriptions << subscription
         new_sub = customer.customer_subscriptions.last
-
+        expect(new_sub.status).to eq('active')
         body = { status: "cancelled" }
         patch "/api/v1/customer_subscriptions/#{new_sub.id}", params: body
         expect(response).to be_successful
         result = JSON.parse(response.body, symbolize_names: true)
-
+        new_sub.reload
+        expect(new_sub.status).to eq('cancelled')
         data = result[:data]
         expect(data).to be_a Hash
         expect(data).to have_key :id
@@ -27,12 +28,26 @@ RSpec.describe 'updating a customer_subscription' do
         expect(attributes).to have_key :subscription_id
         expect(attributes).to have_key :frequency
         expect(attributes).to have_key :status
-        expect(attributes[:status]).to eq("cancelled")
+        expect(attributes[:status]).to eq('cancelled')
       end
     end
 
     describe 'sad path' do
+      describe 'if status is passed incorrectly' do
+        it 'returns a 304 and an error' do
+          customer.subscriptions << subscription
+          new_sub = customer.customer_subscriptions.last
+          expect(new_sub.status).to eq('active')
 
+          body = { status: "in progress" }
+          patch "/api/v1/customer_subscriptions/#{new_sub.id}", params: body
+
+          expect(response).to have_http_status 304
+
+          new_sub.reload
+          expect(new_sub.status).to eq('active')
+        end
+      end
     end
   end
 end
